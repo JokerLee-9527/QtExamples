dda1452dd0094cfc40651ba9f2467c034d22aef7
JokerLee <791565455@qq.com>
2025-12-29 13:51:03 +0800

    QCLASSINFO

--- Q_CLASSINFO_demo/qclassinfodemowindow.cpp
+++ Q_CLASSINFO_demo/qclassinfodemowindow.cpp
@@ -5,6 +5,7 @@
 #include <QMetaObject>
 #include <QMetaClassInfo>
 
+
 QClassInfoDemoWindow::QClassInfoDemoWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::QClassInfoDemoWindow)
@@ -20,12 +21,13 @@ QClassInfoDemoWindow::~QClassInfoDemoWindow()
 void QClassInfoDemoWindow::on_classInfoPushButton_clicked()
 {
 
+
     const QMetaObject* meta = this->metaObject();
-    const int classINfCount = meta->classInfoCount();
+    const int classInfoCount = meta->classInfoCount();
 
 
-    QString editText = QString("Q_CLASSINFO的数量:%1 \n").arg(classINfCount);
-    for (int i = 0; i < classINfCount; i++) {
+    QString editText = QString("Q_CLASSINFO的数量:%1 \n").arg(classInfoCount);
+    for (int i = 0; i < classInfoCount; i++) {
         QMetaClassInfo info = meta->classInfo(i);
         editText.append("name:").append(info.name())\
             .append(" vlaue:").append(info.value()).append("\n");
